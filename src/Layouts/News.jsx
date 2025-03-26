e3a0a6eb37d5693fdfa1684b196e4c6b6e23dcda
Konrad Bojanecki <kbojanecki@gmail.com>
2025-03-26 10:11:08 +0100

    .

--- src/Layouts/News.jsx
+++ src/Layouts/News.jsx
@@ -31,62 +31,76 @@ const data = [
         link: "/",
         image: "https://picsum.photos/150/150"
     },
-    // {
-    //     id: 4,
-    //     dateOfCreate: "2024-03-20",
-    //     title: "Rekrutacja na stanowisko recepcjonisty",
-    //     link: "/",
-    //     image: "https://picsum.photos/150/150"
-    // },
-    // {
-    //     id: 5,
-    //     dateOfCreate: "2024-03-25",
-    //     title: "Nowy oddział kardiologii otwarty",
-    //     link: "/",
-    //     image: "https://picsum.photos/150/150"
-    // },
-    // {
-    //     id: 6,
-    //     dateOfCreate: "2024-03-30",
-    //     title: "Zapraszamy na dni otwarte szpitala",
-    //     link: "/",
-    //     image: "https://picsum.photos/150/150"
-    // },
-    // {
-    //     id: 7,
-    //     dateOfCreate: "2024-04-01",
-    //     title: "Szczepienia przeciwko grypie - terminy",
-    //     link: "/",
-    //     image: "https://picsum.photos/150/150"
-    // },
-    // {
-    //     id: 8,
-    //     dateOfCreate: "2024-04-05",
-    //     title: "Nowoczesne metody leczenia w naszej placówce",
-    //     link: "/",
-    //     image: "https://picsum.photos/150/150"
-    // },
-    // {
-    //     id: 9,
-    //     dateOfCreate: "2024-04-10",
-    //     title: "Bezpłatne konsultacje dietetyczne",
-    //     link: "/",
-    //     image: "https://picsum.photos/150/150"
-    // }
+    {
+        id: 4,
+        dateOfCreate: "2024-03-20",
+        title: "Rekrutacja na stanowisko recepcjonisty",
+        link: "/",
+        image: "https://picsum.photos/150/150"
+    },
+    {
+        id: 5,
+        dateOfCreate: "2024-03-25",
+        title: "Nowy oddział kardiologii otwarty",
+        link: "/",
+        image: "https://picsum.photos/150/150"
+    },
+    {
+        id: 6,
+        dateOfCreate: "2024-03-30",
+        title: "Zapraszamy na dni otwarte szpitala",
+        link: "/",
+        image: "https://picsum.photos/150/150"
+    },
+    {
+        id: 7,
+        dateOfCreate: "2024-04-01",
+        title: "Szczepienia przeciwko grypie - terminy",
+        link: "/",
+        image: "https://picsum.photos/150/150"
+    },
+    {
+        id: 8,
+        dateOfCreate: "2024-04-05",
+        title: "Nowoczesne metody leczenia w naszej placówce",
+        link: "/",
+        image: "https://picsum.photos/150/150"
+    },
+    {
+        id: 9,
+        dateOfCreate: "2024-04-10",
+        title: "Bezpłatne konsultacje dietetyczne",
+        link: "/",
+        image: "https://picsum.photos/150/150"
+    }
 ];
 
 
 const News = () => {
+    let newsList = data.slice(0, 4).map(item => (
+        <SingleNews key={item.id} item={item}/>))
+
+    const visableNews = 4;
+
+    const changeNews = (type) => {
+        if (type === "back") {
+            newsList = data.slice(visableNews, (visableNews - visableNews)).map(item => (
+                <SingleNews key={item.id} item={item}/>));
+        } else if (type === "next") {
+            newsList = data.slice(visableNews, (visableNews + visableNews)).map(item => (
+                <SingleNews key={item.id} item={item}/>));
+        }
+    };
+
     return (
         <section className="section-news-container">
             <div className="section-news">
                 <span className="secondary-color upperCaseText font-heavy section-news-title">Aktualności</span>
                 <div className="container">
-                    {data.map(item=>(
-                        <SingleNews key={item.id} item={item} />
-                    ))}
+                    <button onClick={() => changeNews("back")} className="btn-slider-news">{"<"}</button>
+                    {newsList}
+                    <button onClick={() => changeNews("next")} className="btn-slider-news">{">"}</button>
                 </div>
-
             </div>
         </section>
     )
