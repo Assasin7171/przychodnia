5e39265460ae23d403baaddde0df6b50183c131c
Konrad Bojanecki <KBojanecki@gmail.com>
2025-04-11 15:51:08 +0200

    Edited mobile styles in Header.css

--- src/Layouts/Header.jsx
+++ src/Layouts/Header.jsx
@@ -10,36 +10,50 @@ const Header = () => {
     const [showDropdownMenuMobile, setShowDropdownMenuMobile] = useState({
         "display": "none",
     });
+
+    const [showBackMenuButton, setShowBackMenuButton] = useState(false);
     const handleShowDropdownMenu = (type) => {
-        if(type === "mobile") {
-            setShowDropdownMenuMobile({"display": "flex"});
-        } else if(type === "desktop"){
+        if (type === "desktop") {
             setShowDropdownMenu({"display": "flex"});
         }
-
     };
 
 
     const handleHideDropdownMenu = (type) => {
-        if(type === "mobile") {
-            setShowDropdownMenuMobile({"display": "none"});
-        } else if(type === "desktop") {
+        if (type === "desktop") {
             setShowDropdownMenu({"display": "none"});
         }
     };
+
+
+    const handleShowDropdownMenuMobile = () => {
+        if(!showBackMenuButton){
+            setShowDropdownMenuMobile({"display": "flex"});
+            setShowBackMenuButton(true);
+        } else {
+            setShowBackMenuButton(false);
+            setShowDropdownMenuMobile({"display": "none"});
+        }
+
+    };
+
+
     return (
         <header className="header"
                 onMouseLeave={handleHideDropdownMenu}>
             <div className="header-details">
-                <div className="header-mobile-menu" onClick={()=> handleShowDropdownMenu("mobile")}>
-                    <i className="fa fa-bars"></i>
+                <div className="header-mobile-menu"
+                     onClick={() => handleShowDropdownMenuMobile()}>
+                    {showBackMenuButton ?
+                        <i className="fa fa-arrow-alt-circle-left"></i> :
+                        <i className="fa fa-bars"></i>}
                 </div>
                 <div className="logo">
                     tu będzie logo
                 </div>
                 <div className="nav-links">
                     <div className="links">
-                        <button onMouseOver={()=> handleShowDropdownMenu("desktop")}>
+                        <button onMouseOver={() => handleShowDropdownMenu("desktop")}>
                             usługi
                         </button>
                         <Link onMouseOver={handleHideDropdownMenu} to={"/news"}>aktualności</Link>
@@ -49,8 +63,8 @@ const Header = () => {
                 </div>
 
                 <div className="nav-links-dropdown-menu"
-                     onMouseOver={()=> handleShowDropdownMenu("desktop")}
-                     onMouseLeave={()=> handleHideDropdownMenu("desktop")}
+                     onMouseOver={() => handleShowDropdownMenu("desktop")}
+                     onMouseLeave={() => handleHideDropdownMenu("desktop")}
                      style={showDropdownMenu}>
                     <Link to={"/"}>e - rejestracja</Link>
                     <Link to={"/"}>poradnie specjalistyczne</Link>
@@ -72,22 +86,24 @@ const Header = () => {
             </div>
 
             <div className="nav-links-dropdown-menu-mobile"
-                 onMouseOver={()=> handleShowDropdownMenu("mobile")}
-                 onMouseLeave={()=> handleHideDropdownMenu("mobile")}
+                 onMouseOver={() => handleShowDropdownMenu("mobile")}
+                 onMouseLeave={() => handleHideDropdownMenu("mobile")}
                  style={showDropdownMenuMobile}>
                 <p>Usługi</p>
                 <div className="dropdown-menu-mobile-container">
-                    <Link to={"/"}> e - rejestracja</Link>
+                    <Link to={"/"}>e - rejestracja</Link>
                     <Link to={"/"}>poradnie specjalistyczne</Link>
                     <Link to={"/"}>nasze oddziały</Link>
                     <Link to={"/"}>rehabilitacja</Link>
                     <Link to={"/"}>badania diagnostyczne</Link>
                     <Link to={"/"}>badania klicznie</Link>
                     <Link to={"/"}>e-wyniki</Link>
-                    <Link to={"/"}>aktualności</Link>
                 </div>
-                <Link to={"/"}>o nas</Link>
-                <Link to={"/"}>kontakt</Link>
+                <div className="main-links">
+                    <Link className="border-bottom-links" to={"/"}>aktualności</Link>
+                    <Link className="border-bottom-links" to={"/"}>o nas</Link>
+                    <Link to={"/"}>kontakt</Link>
+                </div>
             </div>
         </header>
 
