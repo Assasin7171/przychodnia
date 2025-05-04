cda0a9e25d02c8ea13b2e3434c2ebb958d5911bd
Konrad Bojanecki <KBojanecki@gmail.com>
2025-05-04 16:59:48 +0200

    Big change in <Header/>,<Banner/>,<Categories/>,<News/>,<Footer/>

--- src/Layouts/Header.jsx
+++ src/Layouts/Header.jsx
@@ -1,61 +1,65 @@
 ﻿import {Link} from "react-router-dom";
-import 'bootstrap/js/src/dropdown.js'
+import 'bootstrap/js/src/dropdown.js';
+import 'bootstrap/js/src/collapse.js';
 
 const Header = () => {
     return (
-        <div className="container-md px-0">
-            <nav className="navbar navbar-expand-lg bg-white sticky-top lead text-uppercase">
-                <div className="container-fluid">
-                    <img src="https://placehold.co/80x80" className="img-fluid" alt="logo"/>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav mx-auto mb-2 mb-lg-0 gap-4">
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                                   aria-expanded="false">
-                                    Usługi
-                                </a>
+        <div className="container-fluid bg-white sticky-top shadow-sm">
+            <div className="container-md px-0 ">
+                <nav className="navbar navbar-expand-lg bg-white  lead text-uppercase">
+                    <div className="container-fluid">
+                        <img src="https://placehold.co/80x80" className="img-fluid" alt="logo"/>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                                aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav mx-auto mb-2 mb-lg-0 gap-4">
+                                <li className="nav-item dropdown">
+                                    <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                       aria-expanded="false">
+                                        Usługi
+                                    </a>
 
-                                <div className="dropdown-menu">
-                                    <div className="container-fluid">
-                                        <div className="row">
-                                            <a className="dropdown-item" href="#">E-Rejestracja</a>
-                                            <a className="dropdown-item" href="#">Poradnie specjalistyczne</a>
-                                            <a className="dropdown-item" href="#">Nasze oddziały</a>
-                                            <a className="dropdown-item" href="#">Stomatologia</a>
+                                    <div className="dropdown-menu">
+                                        <div className="container-fluid">
+                                            <div className="row">
+                                                <a className="dropdown-item" href="#">E-Rejestracja</a>
+                                                <a className="dropdown-item" href="#">Poradnie specjalistyczne</a>
+                                                <a className="dropdown-item" href="#">Nasze oddziały</a>
+                                                <a className="dropdown-item" href="#">Stomatologia</a>
+                                            </div>
                                         </div>
+
+                                        <li><a className="dropdown-item" href="#">Rehabilitacja</a></li>
+                                        <li><a className="dropdown-item" href="#">Badania diagnostyczne</a></li>
+                                        <li><a className="dropdown-item" href="#">Badania kliniczne</a></li>
+                                        <li><a className="dropdown-item" href="#">E-Wyniki</a></li>
                                     </div>
 
-                                    <li><a className="dropdown-item" href="#">Rehabilitacja</a></li>
-                                    <li><a className="dropdown-item" href="#">Badania diagnostyczne</a></li>
-                                    <li><a className="dropdown-item" href="#">Badania kliniczne</a></li>
-                                    <li><a className="dropdown-item" href="#">E-Wyniki</a></li>
-                                </div>
-
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" aria-current="page" to="/aktualnosci">Aktualności</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" aria-current="page" to="/o_nas">O nas</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" aria-current="page" to="/kontakt">Kontakt</Link>
-                            </li>
-                        </ul>
-                        <form className="d-flex" role="search">
-                            <input className="form-control me-2" type="search" placeholder="Search"
-                                   aria-label="Search"/>
-                            <button className="btn btn-outline-success" type="submit">Search</button>
-                        </form>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" aria-current="page" to="/aktualnosci">Aktualności</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" aria-current="page" to="/o_nas">O nas</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" aria-current="page" to="/kontakt">Kontakt</Link>
+                                </li>
+                            </ul>
+                            <form className="d-flex" role="search">
+                                <input className="form-control me-2" type="search" placeholder="Search"
+                                       aria-label="Search"/>
+                                <button className="btn btn-outline-success" type="submit">Search</button>
+                            </form>
+                        </div>
                     </div>
-                </div>
-            </nav>
+                </nav>
+            </div>
         </div>
+
 
 
     )
