808358e71d24faf0b22aa122901ad1db4ffad4c9
Konrad Bojanecki <KBojanecki@gmail.com>
2025-05-16 13:53:45 +0200

    Changes in Banner.jsx

--- src/Components/Tile.jsx
+++ src/Components/Tile.jsx
@@ -9,7 +9,7 @@ const Tile = (props) => {
         //     </div>
         // </Link>
 
-    <Link to="/rejestracja" className="card border-dark col-sm-4 col-lg-3 link_card flex-grow-1 text-decoration-none">
+    <Link to="/rejestracja" className="card col-sm-4 col-lg-3 link_card flex-grow-1 text-decoration-none">
         <div className="card-body d-flex flex-column justify-content-between link_card_body text-primary">
             <h5 className="card-title">{props.header}</h5>
             <p className="card-text text-uppercase">{props.body}</p>
