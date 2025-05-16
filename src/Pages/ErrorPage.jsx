617116e0c246f8c23f6544b9261d0c08506ab71f
Konrad Bojanecki <KBojanecki@gmail.com>
2025-05-16 14:05:40 +0200

    Changes in ErrorPage.jsx

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,14 +1,20 @@
 import "../Styles/ErrorPage.css"
+import {Link} from "react-router-dom";
 
 const ErrorPage = () => {
     return (
-        <section>
-            <div>
-                <h1>Coś poszło nie tak...</h1>
-            </div> 
-        </section>
-
-    )
-}
+        <div className="d-flex flex-column justify-content-center align-items-center vh-100 bg-light text-center px-3">
+            <div className="p-4 shadow bg-white rounded">
+                <h1 className="display-4 text-primary mb-3">🔧 Strona w budowie</h1>
+                <p className="lead mb-4">
+                    Pracujemy nad tą stroną. Wkrótce będzie dostępna!
+                </p>
+                <Link to="/przychodnia" className="btn btn-primary">
+                    Wróć na stronę główną
+                </Link>
+            </div>
+        </div>
+    );
+};
 
 export default ErrorPage
